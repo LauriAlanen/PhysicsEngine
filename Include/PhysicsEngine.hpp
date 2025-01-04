dea79e7d2264b8211943732dd850b2c4006ce3dc
lauala <lauala@lauala.fi>
2025-01-04 19:08:55 +0200

    Added struct for window bounds

--- Include/PhysicsEngine.hpp
+++ Include/PhysicsEngine.hpp
@@ -6,19 +6,29 @@
 #include <SDL2/SDL.h>
 #include <SimulatableObjects.hpp>
 
+struct Bounds {
+    int x_min;  
+    int y_min;  
+    int x_max;  
+    int y_max;
+};
+
 class PhysicsEngine
 {
 private:
     Uint64 previousTime;
     double accumulator;
     std::vector<std::unique_ptr<SimulatableObject>> simulatableObjects;
+    Bounds simulationBounds;
 
 public:
     double timestep;
-    PhysicsEngine(float fps);
+    PhysicsEngine(float fps, Bounds simulationBounds);
     ~PhysicsEngine();
     void update();
     void addSimulatableObject(std::unique_ptr<SimulatableObject> object);
+    void checkBounds(std::unique_ptr<SimulatableObject> &object);
     std::vector<std::unique_ptr<SimulatableObject>>& getSimulatableObjects();
+
 };
 
