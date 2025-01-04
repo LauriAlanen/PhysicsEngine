2445777c1346ce1eea80f5b653cd4fa561367f78
lauala <lauala@lauala.fi>
2025-01-04 19:09:25 +0200

    Add shared variables for all simulatable objects

--- Include/SimulatableObjects.hpp
+++ Include/SimulatableObjects.hpp
@@ -3,24 +3,21 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-#define PARTICLE_SIZE 10
+#define PARTICLE_SIZE 1
 
 class SimulatableObject
 {
 public:
     virtual ~SimulatableObject() = default;
     virtual void update(float deltaTime) = 0;
-    int x_coordinate, y_coordinate;
+    int x, y;
+    float mass, gravity = -9.81f, vy, vx;
 };
 
 class Particle : public SimulatableObject
 {
-private:
-    float position, velocity, gravity = 9.81f;
-
 public:
-    Particle(float position, float velocity);
+    Particle(int x, int y, float vx, float vy);
     ~Particle();
     void update(float deltaTime) override;
 };
- 
