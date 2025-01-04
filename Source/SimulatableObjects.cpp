08a1e4a68200d99d0d6628d56365a0cb6e344de2
lauala <lauala@lauala.fi>
2025-01-04 19:10:36 +0200

    Added basic gravity

--- Source/SimulatableObjects.cpp
+++ Source/SimulatableObjects.cpp
@@ -1,9 +1,13 @@
 #include <SimulatableObjects.hpp>
 
-Particle::Particle(float position, float velocity)
+#define DEBUG
+
+Particle::Particle(int x, int y, float vx, float vy)
 {
-    this->position = position;
-    this->velocity = velocity;
+    this->x = x;
+    this->y = y;
+    this->vx = vx;
+    this->vy = vy;
 }
 
 Particle::~Particle()
@@ -13,10 +17,10 @@ Particle::~Particle()
 
 void Particle::update(float deltaTime)
 {
-    this->velocity += this->gravity * deltaTime;
-    this->position += this->velocity * deltaTime;
-    this->x_coordinate = position;
-    this->y_coordinate = velocity;
-    std::cout << "X is : " << this->x_coordinate << std::endl;
-    std::cout << "Y is : " << this->y_coordinate << std::endl;
+    this->vy += this->gravity * deltaTime;
+    this->y += vy * deltaTime;
+
+    #ifdef DEBUG
+    std::cout << "X, Y is : " << this->x << ", " << this->y << std::endl;
+    #endif
 }
