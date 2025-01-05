95daa8eab0559767e140569cfa60a0d7ec1972c1
lauala <lauala@lauala.fi>
2025-01-05 13:44:53 +0200

    Added basic properties to all simulatable objects.

--- Include/SimulatableObjects.hpp
+++ Include/SimulatableObjects.hpp
@@ -3,21 +3,39 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-#define PARTICLE_SIZE 10
+#define PARTICLE_SIZE 2
+
+class BasicState {
+public:
+    int x, y;
+    float vx, vy;
+    float ax, ay;  
+    float mass;
+    float gravity = -9.81f;
+
+    BasicState(int x = 0, int y = 0, 
+          float vx = 0, float vy = 0, 
+          float mass = 1.0f)
+        : x(x), y(y), vx(vx), vy(vy), mass(mass) {}
+};
+
 
 class SimulatableObject
 {
 public:
     virtual ~SimulatableObject() = default;
     virtual void update(float deltaTime) = 0;
-    int x, y;
-    float mass, gravity = -9.81f, vy, vx;
+    BasicState currentState, previousState;
 };
 
 class Particle : public SimulatableObject
 {
 public:
-    Particle(int x, int y, float vx, float vy);
+    Particle(int x, int y, float vx, float vy, float mass)
+    {
+        this->currentState = BasicState(x, y, vx, vy, mass);
+    }
+
     ~Particle();
     void update(float deltaTime) override;
 };
